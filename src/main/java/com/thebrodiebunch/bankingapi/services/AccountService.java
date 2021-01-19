c581ce12e221f2f730ffe7959820e6d8b75f1add
Brian Conlon <brian.conlon18@myhunter.cuny.edu>
2021-01-19 16:56:51 -0500

    Annotations

--- src/main/java/com/thebrodiebunch/bankingapi/services/AccountService.java
+++ src/main/java/com/thebrodiebunch/bankingapi/services/AccountService.java
@@ -1,10 +1,20 @@
 package com.thebrodiebunch.bankingapi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import com.thebrodiebunch.bankingapi.data.AccountDAO;
+
+@Service
 public class AccountService {
-//	public Account addAcount(Account a);
-//	public Set<Account> getAccounts();
-//	public Account getAccountById(Integer id);
-//	public Account getAccountByName(String username);
-//	public void deleteAccount(Account a);
-//	public void updateAccount(Account a);
+	@Autowired
+	AccountDAO accountDao;
+	
+	public Account addAccount(Account a);
+	public Set<Account> getAccounts();
+	public Account getAccountById(Integer id);
+	public Account getAccountByName(String username);
+	public void deleteAccount(Account a);
+	public void updateAccount(Account a);
 }
--- src/main/java/com/thebrodiebunch/bankingapi/services/UserService.java
+++ src/main/java/com/thebrodiebunch/bankingapi/services/UserService.java
@@ -1,6 +1,14 @@
 package com.thebrodiebunch.bankingapi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thebrodiebunch.bankingapi.data.UserDAO;
+
+@Service
 public class UserService {
+	@Autowired
+	UserDAO userDao;
 //	public User addUser(User u); //throws NonUniqueUsernameException ; for SubmitAccount endpoint
 //	public Set<User> getUsers();
 //	public User getUserById(Integer id);
