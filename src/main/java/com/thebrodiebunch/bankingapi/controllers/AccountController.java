91fbcfbff3426ca7acf36897ec4f436a74a73138
Jason Zelonka <jzelonka@ufl.edu>
2021-01-19 18:02:05 -0500

    basic setup, no security

--- src/main/java/com/thebrodiebunch/bankingapi/controllers/AccountController.java
+++ src/main/java/com/thebrodiebunch/bankingapi/controllers/AccountController.java
@@ -1,19 +1,76 @@
 package com.thebrodiebunch.bankingapi.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thebrodiebunch.bankingapi.Beans.Account;
+import com.thebrodiebunch.bankingapi.services.AccountService;
 
 @RequestMapping("/accounts")
 @RestController
 public class AccountController {
 
-	@GetMapping
-	public ResponseEntity<Account[]> getAccounts()
+	private AccountService accServ;
+	
+	@Autowired
+	public AccountController(AccountService accServ)
 	{
-		
+		this.accServ = accServ;
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<Account>> getAccounts()
+	{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return ResponseEntity.ok(accServ.getAccounts());
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Account> getAccountById(@PathVariable Integer id)
+	{
+		Account acc = accServ.getAccountById(id);
+		if (acc != null)
+		{
+			return ResponseEntity.ok(acc);
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
+	@GetMapping("/status/{statusId}")
+	public ResponseEntity<List<Account>> getAccountsByStatus(@PathVariable Integer statusId)
+	{
+		return ResponseEntity.ok(accServ.getAccountByStatus(statusId));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Account> addAccount(@RequestBody Account acc)
+	{
+		if(acc.getId() != 0)
+		{
+			return ResponseEntity.badRequest().build();
+		}
+		
+		return ResponseEntity.ok(accServ.addAcount(acc));
+	}
+	
+	
+	@PutMapping
+	public ResponseEntity<Account> updateAccount(@RequestBody Account acc)
+	{
+		accServ.updateAccount(acc);
+		return ResponseEntity.ok(acc);
+	}
+	
+	
 }
--- src/main/java/com/thebrodiebunch/bankingapi/services/AccountService.java
+++ src/main/java/com/thebrodiebunch/bankingapi/services/AccountService.java
@@ -1,10 +1,58 @@
 package com.thebrodiebunch.bankingapi.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thebrodiebunch.bankingapi.Beans.Account;
+import com.thebrodiebunch.bankingapi.data.AccountDAO;
+
+@Service
 public class AccountService {
-//	public Account addAcount(Account a);
-//	public Set<Account> getAccounts();
-//	public Account getAccountById(Integer id);
-//	public Account getAccountByName(String username);
-//	public void deleteAccount(Account a);
-//	public void updateAccount(Account a);
+	private AccountDAO adao;
+	
+	
+	@Autowired
+	public AccountService(AccountDAO adao)
+	{
+		this.adao = adao;
+	}
+	
+	public Account addAcount(Account a)
+	{
+		return adao.save(a);
+	}
+	public List<Account> getAccounts()
+	{
+		return adao.findAll();
+	}
+	public Account getAccountById(Integer id)
+	{
+		return adao.getOne(id);
+	}
+	public List<Account> getAccountByStatus(Integer statusId)
+	{
+		List<Account> all = adao.findAll();
+		all.removeIf(account -> account.getStatus().getId() != statusId);
+		return all;
+	}
+	public void deleteAccount(Account a)
+	{
+		adao.delete(a);
+	}
+	public void updateAccount(Account a)
+	{
+		Account instance = adao.getOne(a.getId());
+		if (instance != null)
+		{
+			adao.save(a);
+		}
+	}
+	public List<Account> getAccountbyUser(Integer userId)
+	{
+		List<Account> all = adao.findAll();
+		all.removeIf(account -> account.getOwnerId() != userId);
+		return all;
+	}
 }
